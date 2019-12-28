71c83c5edc82fc4530ddd922ca1f537db876fdbe
cmpmq <52877827+cmpmq@users.noreply.github.com>
2019-12-28 23:25:04 +0800

    Add files via upload

--- projectracer/project_tracer.js
+++ projectracer/project_tracer.js
@@ -45,27 +45,69 @@ const task = [
 	}
 ]
 
-console.log(task)
+
+
+console.log(task)  //arrays of objects
 let x = new Date().toDateString()
 task[0].date[1] = new Date().toDateString()
 console.log(x)
 
 // getelementbyid
-console.log(document.getElementById('tasks'))
+// console.log(document.getElementById('tasks'))
 
-// getelementbyclassname
-// ul gets a [htmlcollection] - is an array
-var taskNames = document.getElementsByClassName('task-name')
-var progresses = document.getElementsByClassName('task-progress')
-var taskDays = document.getElementsByClassName('task-day')
-// console.log(taskNames)
-// console.log(taskNames[1])
-// taskNames[0].textContent = task[0].name
+function createTask(){
+	// create element
+	var newDiv = document.createElement('div')
+	newDiv.className = "task-item"
+	// var newDivText = document.createTextNode('New Task')
+	// newDiv.appendChild(newDivText)
+	var taskList = document.getElementById('tasks')
+	taskList.appendChild(newDiv)
+	
+	var newSpan = document.createElement('span')
+	newSpan.className = "task-name"
+	var newSpanText = document.createTextNode('New Task')
+	newSpan.appendChild(newSpanText)
+	newDiv.appendChild(newSpan)
+	
+	var newInputPlus = document.createElement('input')
+	newInputPlus.type = 'button'
+	newInputPlus.value = '+'
+	newDiv.appendChild(newInputPlus)
+	
+	var newInputDetail = document.createElement('input')
+	newInputDetail.type = 'button'
+	newInputDetail.value = '···'
+	newDiv.appendChild(newInputDetail)
+	
+	var newlist = document.createElement('ul')
+	newDiv.appendChild(newlist)
+	
+	var newlipro = document.createElement('li')
+	newlipro.className = "task-progress"
+	var newliproText = document.createTextNode('sum / total; Current Percentage')
+	newlipro.appendChild(newliproText)
+	newDiv.appendChild(newlipro)
+	
+	var newliday = document.createElement('li')
+	newliday.className = "task-day"
+	var newlidayText = document.createTextNode('xd 平均; yd 当前')
+	newliday.appendChild(newlidayText)
+	newDiv.appendChild(newliday)
+}
 
-// let ltask = task.length // get object array length
-// console.log(task[1].name)
-// process and print data
-for (var i = 0; i < task.length; i++){
+function TaskWrite(i){
+	// getelementbyclassname
+	// ul gets a [htmlcollection] - is an array
+	var taskNames = document.getElementsByClassName('task-name')
+	var progresses = document.getElementsByClassName('task-progress')
+	var taskDays = document.getElementsByClassName('task-day')
+	 // console.log(taskNames)
+	// console.log(taskNames[1])
+	// taskNames[0].textContent = task[0].name
+	
+	
+	// objects of arrays
 	taskNames[i].textContent = task[i].name
 	task[i].percentage = toPercentage(task[i].sum, task[i].total)
 	progresses[i].textContent = `${task[i].sum} / ${task[i].total}; ${task[i].percentage}`
@@ -76,8 +118,50 @@ for (var i = 0; i < task.length; i++){
 	let cspeed = task[i].delta[days - 1]
 	task[i].dcurrent = Math.round((task[i].total - task[i].sum) / cspeed)
 	taskDays[i].textContent = task[i].daverage + " 平均; " + task[i].dcurrent + " 当前"
+	
+}
+
+// let ltask = task.length // get object array length
+// console.log(task[1].name)
+// process and print data
+for (var i = 0; i < task.length; i++){
+	
+	createTask()
+	TaskWrite(i)
 }
 
 
-console.log(task[0].delta)
-console.log(task[0].date)
+// console.log(task[0].delta)
+// console.log(task[0].date)
+
+var taskNum = document.getElementsByClassName('task-item').length
+console.log(document.getElementsByClassName('task-item'))
+console.log(taskNum)
+
+ // console.log(newDiv)
+
+
+function submitTask(){
+	console.log('function submit')
+	var taskname = document.getElementById('taskname').value
+	var totalamount = document.getElementById('totalamount').value
+	var sumamount = document.getElementById('sumamount').value
+	task.push({
+		id: 3,
+		name: taskname,
+		sum: sumamount,
+		total: totalamount,
+		percentage: 0,
+		isCompleted: false,
+		date: [],
+		delta: [sumamount],
+		daverage: 0,
+		dcurrent: 0
+	})
+	
+	createTask()
+	// console.log(taskNum)
+	TaskWrite(taskNum)
+	taskNum = document.getElementsByClassName('task-item').length
+	// console.log(taskNum)
+}
